11da9742fee922375da41747527d38752a62e7a8
Firee <newowner@News-MacBook-Pro.local>
2026-05-01 20:21:51 -0400

    add useDemounce and use pagination components demos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import DebounceSearchDemo from "./components/DebounceSearchDemo.jsx";
+
+import PaginationDemo from "./components/PaginationDemo.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>🪝 Coustom Hooks</h1>
+      <h2>useDebounce</h2>
+      <DebounceSearchDemo />
+      <h2>usePagination</h2>
+      <PaginationDemo />
+    </div>
+  );
 }
 
 export default App;
